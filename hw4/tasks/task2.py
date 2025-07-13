21c4fafb62f26492247c5c475237c14bf8414c88
Maxim Zinkov <zinkov.maxim@gmail.com>
2025-07-13 18:00:28 +0300

    solution task2

--- hw4/tasks/task2.py
+++ hw4/tasks/task2.py
@@ -4,3 +4,12 @@
 Вам даны две переменные a и b, равные соответственно: 21 и 23
 Распечатайте их сумму и произведение на разных строчках.
 """
+
+a = 21
+b = 23
+
+sum_result = a + b
+multiply_result = a * b
+
+print(sum_result)
+print(multiply_result)
