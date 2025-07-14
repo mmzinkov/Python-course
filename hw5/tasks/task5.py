08d4b9228c7594d2151d21552ecac6be2fe0d0f8
Maxim Zinkov <zinkov.maxim@gmail.com>
2025-07-14 22:30:21 +0300

    solution task5

--- hw5/tasks/task5.py
+++ hw5/tasks/task5.py
@@ -13,3 +13,33 @@
 
 > Гарантируется, что номер месяца является целым числом.
 """
+
+month_number = int(input("Введите номер месяца: "))
+
+match month_number:
+    case 1:
+        print("Январь")
+    case 2:
+        print("Февраль")
+    case 3:
+        print("Март")
+    case 4:
+        print("Апрель")
+    case 5:
+        print("Май")
+    case 6:
+        print("Июнь")
+    case 7:
+        print("Июль")
+    case 8:
+        print("Август")
+    case 9:
+        print("Сентябрь")
+    case 10:
+        print("Октябрь")
+    case 11:
+        print("Ноябрь")
+    case 12:
+        print("Декабрь")
+    case _:
+        print("Номер месяца введен неверно")
